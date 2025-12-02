46a11165bb1dd1aef9ec538b384d04caab43c8b4
Heric Leite Rodrigues <heric.rodrigues@aprix.com.br>
2025-12-01 21:57:14 -0300

    Feat: adding title h2 component

---
+++ weather-report/src/components/title-h2.tsx
@@ -0,0 +1,7 @@
+export function TitleH2({ title }: { title: string }) {
+  return (
+    <h2 className='scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0'>
+      {title}
+    </h2>
+  );
+}
--- weather-report/src/pages/home.tsx
+++ weather-report/src/pages/home.tsx
@@ -1,3 +1,11 @@
+import { TitleH2 } from '@/components/title-h2';
+
+const TITLE = 'Porto Alegre';
+
 export function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <TitleH2 title={TITLE} />
+    </div>
+  );
 }
