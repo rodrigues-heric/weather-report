14b70bbd52479249dd48c519335c3a929ffc5e54
Heric Leite Rodrigues <hericleiterodrigues@gmail.com>
2026-01-22 17:54:39 -0300

    Feat: adding CityControler tests

--- backend/weather/src/city/city.controller.spec.ts
+++ backend/weather/src/city/city.controller.spec.ts
@@ -2,6 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CityController } from './city.controller';
 import { CityService } from './city.service';
 import { JwtAuthGuard } from '../auth/auth.guard';
+import { CityFetchDto } from './dto/city-fetch.dto';
+import { plainToInstance } from 'class-transformer';
+import { City } from './schemas/city.schema';
 
 describe('CityController', () => {
   let controller: CityController;
@@ -9,6 +12,9 @@ describe('CityController', () => {
   const mockCityService = {
     fetchCityData: jest.fn(),
   };
+
+  let dto: CityFetchDto;
+  let cityDto: City;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -26,11 +32,39 @@ describe('CityController', () => {
 
     controller = module.get<CityController>(CityController);
     service = module.get<CityService>(CityService);
+
+    const plainObject = {
+      name: 'Sample City',
+      state: 'Sample State',
+      country: 'Sample Country',
+      lang: 'en',
+    };
+    dto = plainToInstance(CityFetchDto, plainObject);
   });
 
   describe('creating', () => {
     it('should create TestingCityController', () => {
       expect(controller).toBeDefined();
+    });
+  });
+
+  describe('fetchCityData', () => {
+    it('should call cityService.fetchCityData and return a City object', async () => {
+      const mockResult = { name: 'Sample City', temperature: 25 };
+      mockCityService.fetchCityData.mockResolvedValue(mockResult);
+
+      const result = await controller.fetchCityData(dto);
+
+      expect(service.fetchCityData).toHaveBeenCalledWith(dto);
+      expect(result).toEqual(mockResult);
+    });
+
+    it('should return null when cityService returns null', async () => {
+      mockCityService.fetchCityData.mockResolvedValue(null);
+
+      const result = await controller.fetchCityData(dto);
+
+      expect(result).toBeNull();
     });
   });
 });
--- backend/weather/src/city/city.controller.ts
+++ backend/weather/src/city/city.controller.ts
@@ -13,14 +13,19 @@ import { JwtAuthGuard } from '../auth/auth.guard';
 
 @Controller('/city')
 export class CityController {
-  constructor(private readonly cityService: CityService) {}
+  constructor(
+    /* v8 ignore next */
+    private readonly cityService: CityService,
+  ) {}
 
   @UseGuards(JwtAuthGuard)
   @Post('/fetch')
   @HttpCode(HttpStatus.OK)
   async fetchCityData(
+    /* v8 ignore next */
     @Body() cityFetchPayload: CityFetchDto,
-  ): Promise<City | null> {
+  ) /* v8 ignore next */
+  : Promise<City | null> {
     return this.cityService.fetchCityData(cityFetchPayload);
   }
 }
